84ff3a22a53999b6b6699bfa86ed15e978b449cb
Dmitry Zaitsev <122267379+DmitriZaytsev@users.noreply.github.com>
2025-05-12 07:57:53 +0300

    feat: Make Footer and Header

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: 'LayoutFooter'
+};
+</script>
+
+<template>
+  <footer class="footer">
+    <p>Разработал Дмитрий Зайцев</p>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+.footer {
+  background-color: black;
+  color: white;
+  flex-shrink: 0;
+  padding: 1.5rem;
+  display: flex;
+  justify-content: flex-end;
+
+  @media (min-width: $breakpoint-lg) {
+    padding-left: 6.25rem;
+    padding-right: 6.25rem;
+  }
+}
+</style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  name: 'LayoutHeader'
+};
+</script>
+
+<template>
+  <header class="header">
+    <p class="header__title">Todo Project</p>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  background-color: $info;
+  color: #fff;
+  padding: 0 0.9375rem;
+  height: 3.75rem;
+  display: flex;
+  align-items: center;
+
+  &__title {
+    font-size: 1.5rem;
+    font-weight: 600;
+    text-align: left;
+  }
+
+  @media (max-width: $breakpoint-md) {
+    padding: 0 0.625rem;
+    height: 3.125rem;
+
+    &__title {
+      font-size: 1.2rem;
+    }
+  }
+}
+</style>
