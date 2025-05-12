6dd5e5fe6ab831ab95171859274df7d2f1bc392c
Dmitry Zaitsev <122267379+DmitriZaytsev@users.noreply.github.com>
2025-05-12 13:27:28 +0300

    feat: add urls for api requests. add new db

---
+++ src/config/request-urls/index.js
@@ -0,0 +1,5 @@
+export const baseMockTasksUrl = 'http://localhost:3000/tasks';
+
+export const getMockTaskURL = id => `${baseMockTasksUrl}/${id}`;
+
+export const getMockTasksURL = () => baseMockTasksUrl;
